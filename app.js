b450683a05461473211cd5c0383a45a00567010f
ZFRenato <renatopscarlos@gmail.com>
2023-06-15 15:58:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json())
+
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    success: 'true',
+    message: 'healthy teste2 EMAIL', 
+    version: '1.0.0',
+  });
+});
+
+app.post('/email', (req, res) => {
+  const { email, senha } = req.body
+  if (!email || !senha) {
+    res.status(400)
+  }
+  res.status(200).json({
+    success: true,
+    message: "email"
+  })
+})
+
+app.listen(port);
+console.log(`Aplicação executando na porta.. [Email]: ${port}`);
